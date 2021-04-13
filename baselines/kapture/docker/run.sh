2c12e6f22edfd70d6bd5d880b2f71aa5540e72e9
Руслан Владимирович Мусаев <musaev.rv@phystech.edu>
2021-04-13 19:15:13 +0000

    Update run.sh

--- baselines/kapture/docker/run.sh
+++ baselines/kapture/docker/run.sh
@@ -6,5 +6,9 @@ docker run -it --rm \
     --privileged \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v /home/${USER}/iprofihack/baselines/kapture/scripts:/home/${USER}/YaProfi:rw \
+    -v /data/${USER}:/data/${USER}:rw \
+    -v /data_fast/${USER}:/data_fast/${USER}:rw \
+    -v /data_fast/IPROFI:/data_fast/IPROFI:ro \
+
     iprofi_${USER}:latest
 
