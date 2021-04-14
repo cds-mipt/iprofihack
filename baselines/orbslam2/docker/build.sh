968691b2d357f4c5a9033010ec54ec01d2e7ce89
Rusamus <musaev.rv@phystech.edu>
2021-04-14 13:47:54 +0000

    fix permission denied in orbslam on lorien

--- baselines/orbslam2/docker/build.sh
+++ baselines/orbslam2/docker/build.sh
@@ -3,7 +3,7 @@
 docker build .. \
     --network host \
     -f Dockerfile \
-    --build-arg UID=$(id -g) \
+    --build-arg UID=${UID} \
     --build-arg GID=$(id -g) \
     --build-arg USER=${USER} \
     -t orbslam_python_${USER}:latest
