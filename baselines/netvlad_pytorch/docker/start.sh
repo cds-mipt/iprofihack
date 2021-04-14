904ee09d69adb1e5308f5c62cb0c0e613347a0e9
IaroslavS <47471352+IaroslavS@users.noreply.github.com>
2021-04-14 14:43:13 +0300

    Update start.sh

--- baselines/netvlad_pytorch/docker/start.sh
+++ baselines/netvlad_pytorch/docker/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ "$(docker ps -aq -f status=exited -f name=netvlad)" ]; then
-    docker rm netvlad;
+if [ "$(docker ps -aq -f status=exited -f name=netvlad_${USER})" ]; then
+    docker rm netvlad_${USER};
 fi
 
 docker run -it --rm -d\
@@ -9,11 +9,11 @@ docker run -it --rm -d\
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --privileged \
-    --name netvlad \
+    --name netvlad_${USER} \
     --ipc=host \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v `pwd`/../:/home/docker_netvlad:rw \
     -v /data/${USER}:/data/docker_netvlad:rw \
     -v /data_fast/${USER}:/data_fast/docker_netvlad:rw \
     -v /data_fast/IPROFI:/data_fast/IPROFI:ro \
-    x64/netvlad_pytorch_${USER}:latest
+    x64/netvlad_pytorch:latest
